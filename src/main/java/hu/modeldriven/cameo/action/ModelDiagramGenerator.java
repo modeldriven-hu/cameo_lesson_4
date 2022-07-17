1c7951b87bd6c2a2dc854ce2c76ede0b5353ea38
Zsolt Sandor <zsolt.sandor@gmail.com>
2022-07-17 08:37:27 +0200

    Added setting symbol property

--- src/main/java/hu/modeldriven/cameo/action/ModelDiagramGenerator.java
+++ src/main/java/hu/modeldriven/cameo/action/ModelDiagramGenerator.java
@@ -6,8 +6,13 @@ import com.nomagic.magicdraw.openapi.uml.ModelElementsManager;
 import com.nomagic.magicdraw.openapi.uml.PresentationElementsManager;
 import com.nomagic.magicdraw.openapi.uml.ReadOnlyElementException;
 import com.nomagic.magicdraw.openapi.uml.SessionManager;
+import com.nomagic.magicdraw.properties.ColorProperty;
+import com.nomagic.magicdraw.properties.PropertyID;
+import com.nomagic.magicdraw.properties.PropertyManager;
 import com.nomagic.magicdraw.uml.DiagramTypeConstants;
 import com.nomagic.magicdraw.uml.Finder;
+import com.nomagic.magicdraw.uml.symbols.DiagramPresentationElement;
+import com.nomagic.magicdraw.uml.symbols.shapes.ShapeElement;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Package;
 
 import java.awt.*;
@@ -45,12 +50,17 @@ public class ModelDiagramGenerator {
         var diagramPresentation = project.getDiagram(diagram);
         diagramPresentation.open();
 
+        createDiagramElements(parentPackage, diagramPresentation);
+    }
+
+    private void createDiagramElements(Package parentPackage, DiagramPresentationElement diagramPresentation) throws ReadOnlyElementException {
         var firstClass = Finder.byName().find(parentPackage,
                 com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class.class,
                 "First class");
 
         var firstClassShape = presentationManager.createShapeElement(firstClass, diagramPresentation);
         presentationManager.reshapeShapeElement(firstClassShape, new Rectangle(50, 50, 200, 300));
+        setFillColorToRed(firstClassShape);
 
         var secondClass = Finder.byName().find(parentPackage,
                 com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class.class,
@@ -64,6 +74,15 @@ public class ModelDiagramGenerator {
                 "myDependency");
 
         var dependencyPath = presentationManager.createPathElement(dependency, secondClassShape, firstClassShape);
+    }
+
+    private void setFillColorToRed(ShapeElement shapeElement) throws ReadOnlyElementException {
+        var propertyManager = new PropertyManager();
+        var colorProperty = (ColorProperty)shapeElement.getProperty(PropertyID.FILL_COLOR);
+        var clonedColorProperty = colorProperty.clone();
+        clonedColorProperty.setValue(new Color(255,0,0));
+        propertyManager.addProperty(clonedColorProperty);
+        presentationManager.setPresentationElementProperties(shapeElement, propertyManager);
     }
 
 
